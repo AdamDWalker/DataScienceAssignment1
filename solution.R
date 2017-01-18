a60d361884ef663721d67a91ed3e252312fc9f96
AdamDWalker <adamwalker03@googlemail.com>
2017-01-18 14:19:37 +0000

    Added commented notes to solution file

--- solution.R
+++ solution.R
@@ -2,3 +2,28 @@
 
 ds_training <- read.csv("ds_training.csv", header=FALSE)
 View(ds_training)
+
+# DATA PREP
+#     Data Exploring
+#         How many observations/variables
+#         Variable Categories?
+#         Extreme/Missing values?
+
+#         DISTRIBUTIONS???
+#         Present findings - Tables/Plots
+
+#     Data Pre-Processing
+#         Clean the data set - Extreme/Missing values
+#         Standardise/Normalise required? Why?
+
+# MODEL TRAINING
+#     Candidate Models?
+#     Why?
+#     Why split into training and testing sets
+#     What method of splitting?
+#     Are all variables required? Why?
+
+# MODEL EVALUATION
+#     How is a model chosen?
+#     How can the AUC result be improved?
+#     Other pre-processing methods that can improve the AUC results?
